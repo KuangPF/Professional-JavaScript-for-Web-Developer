bf3324d3ebf9f234fd3bbb00ffab60a4708f9c22
kuangpengfei <kuangpengfei@youloft.com>
2018-03-08 10:24:22 +0800

    add eslint & stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,18 @@
+//https://github.com/stylelint/stylelint-config-standard
+module.exports = {
+	extends: "stylelint-config-standard",
+	rules: {
+		// "at-rule-no-unknown": [true, {
+		// 	"ignoreAtRules": [
+		// 		"extends",
+		// 		"ignores"
+		// 	]
+		// }],
+		"indentation": "tab",
+		"rule-empty-line-before":null,
+		"comment-whitespace-inside":null,
+		"comment-empty-line-before":null,
+		// "number-leading-zero": null,
+		// "unit-whitelist": ["em", "rem", "s"]
+	}
+}
