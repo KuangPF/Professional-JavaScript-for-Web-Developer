05197f9f1e6205293b228c57a76a2115c3c139e5
KuangPF <1633397286@qq.com>
2018-04-09 16:03:38 +0800

    inherit #6

--- src/chapter06/inherit-constructor.js
+++ src/chapter06/inherit-constructor.js
@@ -3,8 +3,14 @@
 function SuperType(msg) {
 	this.color = ['green', 'blue', 'yellow'];
 	this.msg = msg;
+	this.getMsg = function() {
+		return this.msg;
+	}
 }
 
+SuperType.prototype.getMsg = function() {
+	return this.msg;
+}
 function SubType(msg) {
 	SuperType.call(this, msg)
 }
@@ -14,4 +20,4 @@ let subType02 = new SubType();
 subType01.color.push('black');
 console.log(subType01.color); // ["green", "blue", "yellow", "black"]
 console.log(subType02.color); // ["green", "blue", "yellow"]
-console.log(subType01.msg); // subType01
+console.log(subType01.getMsg()); // subType01
